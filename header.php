d19e5e4443b29bb90fa53bc867cd9cd25cd9a632
h3 <joaquinbastias321@gmail.com>
2022-06-12 19:58:09 -0300

    Nav Menus

--- header.php
+++ header.php
@@ -3,11 +3,12 @@
 <head>
     <!-- CSS only -->
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous"> -->
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="widht=decie-widht, initial-scale=1">
     <?php wp_head(  )?>
 
 </head>
-<body>
+<body <?php body_class();?>>
     <header class="site-header">
       <div class="container">
         <h1 class="school-logo-text float-left">
@@ -17,8 +18,13 @@
         <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
         <div class="site-header__menu group">
           <nav class="main-navigation">
+            <?php 
+            // wp_nav_menu(array(
+            //   'theme_location'=>'headerMenuLocation'
+            // ))
+            ?>
             <ul>
-              <li><a href="<?php echo site_url( '/about-us' ); ?>">About Us</a></li>
+              <li <?php if(is_page( 'about-us' ) or wp_get_post_parent_id( 0 )==56){echo 'class="current-menu-item"';}?>><a href="<?php echo site_url( '/about-us' ); ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
               <li><a href="#">Events</a></li>
               <li><a href="#">Campuses</a></li>
