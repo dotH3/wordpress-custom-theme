2bde6f4cb9c8ad4ccba44090471f2ab78f6de8a2
h3 <joaquinbastias321@gmail.com>
2022-06-17 14:33:47 -0300

    a

--- single.php
+++ single.php
@@ -1,12 +1,13 @@
-<?php get_header();
+<?php get_header();?>
 
-    while(have_posts(  )){
-        the_post(  ); ?>
-        <h1>Post</h1>
-        <h2>Title: <?php the_title(); ?></h2>
-        <?php the_content(); ?>
-        <hr>
-    <?php }
+    <div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p> //No olvidar agregar el subtitulo </p>
+        </div>
+      </div>
+    </div>
 
-    get_footer();
-?>
+<?php get_footer();?>
