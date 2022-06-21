a2f829ccc06c174f978216fcc355845650edf931
h3 <joaquinbastias321@gmail.com>
2022-06-20 23:39:28 -0300

    new

--- single.php
+++ single.php
@@ -1,5 +1,7 @@
-<?php get_header();?>
+<?php get_header();
 
+while(have_posts()){
+    the_post();?>
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('images/ocean.jpg')?>)"></div>
       <div class="page-banner__content container container--narrow">
@@ -10,4 +12,14 @@
       </div>
     </div>
 
-<?php get_footer();?>
+    <div class="container container--narrow page-section">
+            <div class="metabox metabox--position-up metabox--with-home-link">
+                <p><a class="metabox__blog-home-link" href="<?php echo site_url('/blog');?>"><i class="fa fa-home" aria-hidden="true"></i> Blog Home</a> <span class="metabox__main">Posted By <?php the_author_posts_link();?> on <?php the_time('n.j.y');?> in <?php echo get_the_category_list( ', ' );?></span></p>
+            </div>
+        <div class="generic-content"><?php the_content();?></div>
+    </div>
+    
+    <?php
+}
+
+get_footer();?>
