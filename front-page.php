201afbf078a9af6c072b4bfa6b1e3920a67f1a5b
h3 <joaquinbastias321@gmail.com>
2022-06-27 00:06:05 -0300

    27/06/22

--- front-page.php
+++ front-page.php
@@ -23,9 +23,9 @@
             while($homepageEvent->have_posts()) {
               $homepageEvent->the_post(); ?>
                  <div class="event-summary">
-                    <a class="event-summary__date t-center" href="#">
-                      <span class="event-summary__month">Mar</span>
-                      <span class="event-summary__day">25</span>
+                    <a class="event-summary__date t-center" href="<?php the_permalink();?>">
+                      <span class="event-summary__month"><?php the_time('M')?></span>
+                      <span class="event-summary__day"><?php the_time('d')?></span>
                     </a>
                     <div class="event-summary__content">
                       <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink();?>"><?php the_title();?></a></h5>
@@ -57,7 +57,11 @@
                 </a>
                 <div class="event-summary__content">
                   <h5 class="event-summary__title headline headline--tiny"><?php the_title();?><a href="<?php the_permalink();?>"></a></h5>
-                  <p><?php echo wp_trim_words( get_the_content(), 18 );?><a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
+                  <p><?php if(has_excerpt()) {
+                    echo get_the_excerpt();
+                  } else {
+                    echo wp_trim_words(get_the_content(), 18);
+                  }?><a href="<?php the_permalink();?>" class="nu gray">Read more</a></p>
                 </div>
               </div>
             <?php } wp_reset_postdata();
